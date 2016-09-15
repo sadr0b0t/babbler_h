3adce4bcbf6a23370a042fbb995463396618c833
1i7 <ii1i7@yandex.ru>
2016-09-15 20:47:21 +0300

    Включить в пример с JSON пользовательские команды ledon/ledoff

--- babbler_h/examples/babbler_input_json/babbler_input_json.ino
+++ babbler_h/examples/babbler_input_json/babbler_input_json.ino
@@ -5,18 +5,95 @@
 #include "babbler_serial.h"
 
 
+
+#define LED_PIN 13
+
+/** Реализация команды ledon (включить лампочку) */
+/** ledon (turn led ON) command implementation */
+int cmd_ledon(char* reply_buffer, int argc=0, char *argv[]=NULL) {
+    digitalWrite(LED_PIN, HIGH);
+    
+    // команда выполнена
+    strcpy(reply_buffer, REPLY_OK);
+    return strlen(reply_buffer);
+}
+
+/** Реализация команды ledoff (включить лампочку) */
+/** ledoff (turn led OFF) command implementation */
+int cmd_ledoff(char* reply_buffer, int argc=0, char *argv[]=NULL) {
+    digitalWrite(LED_PIN, LOW);
+    
+    // команда выполнена
+    strcpy(reply_buffer, REPLY_OK);
+    return strlen(reply_buffer);
+}
+
+babbler_cmd_t CMD_LEDON = {
+    /* имя команды */ 
+    /* command name */
+    "ledon",
+    /* указатель на функцию с реализацией команды */ 
+    /* pointer to function with command implementation*/ 
+    &cmd_ledon
+};
+
+babbler_man_t MAN_LEDON = {
+    /* имя команды */ 
+    /* command name */
+    "ledon",
+    /* краткое описание */ 
+    /* short description */
+    "turn led ON",
+    /* руководство */ 
+    /* manual */
+    "SYNOPSIS\n"
+    "    ledon\n"
+    "DESCRIPTION\n"
+    "Turn led ON."
+};
+
+babbler_cmd_t CMD_LEDOFF = {
+    /* имя команды */ 
+    /* command name */
+    "ledoff",
+    /* указатель на функцию с реализацией команды */ 
+    /* pointer to function with command implementation*/ 
+    &cmd_ledoff
+};
+
+babbler_man_t MAN_LEDOFF = {
+    /* имя команды */ 
+    /* command name */
+    "ledoff",
+    /* краткое описание */ 
+    /* short description */
+    "turn led OFF",
+    /* руководство */ 
+    /* manual */
+    "SYNOPSIS\n"
+    "    ledoff\n"
+    "DESCRIPTION\n"
+    "Turn led OFF."
+};
+
 /** Зарегистрированные команды */
 /** Registered commands */
 extern const babbler_cmd_t BABBLER_COMMANDS[] = {
     // команды из babbler_cmd_core.h
     // commands from babbler_cmd.core.h
     CMD_HELP,
-    CMD_PING
+    CMD_PING,
+    
+    // пользовательские команды
+    // custom commands
+    CMD_LEDON,
+    CMD_LEDOFF
 };
 
 /** Количество зарегистрированных команд */
 /** Number of registered commands*/
 extern const int BABBLER_COMMANDS_COUNT = sizeof(BABBLER_COMMANDS)/sizeof(babbler_cmd_t);
+
 
 /** Руководства для зарегистрированных команд */
 /** Manuals for registered commands */
@@ -24,12 +101,18 @@ extern const babbler_man_t BABBLER_MANUALS[] = {
     // команды из babbler_cmd_core.h
     // commands from babbler_cmd.core.h
     MAN_HELP,
-    MAN_PING
+    MAN_PING,
+    
+    // пользовательские команды
+    // custom commands
+    MAN_LEDON,
+    MAN_LEDOFF
 };
 
 /** Количество руководств для зарегистрированных команд */
 /** Number of manuals for registered commands */
 extern const int BABBLER_MANUALS_COUNT = sizeof(BABBLER_MANUALS)/sizeof(babbler_man_t);
+
 
 
 /**
